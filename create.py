0801991fb03f67f95694c1f68f704a539d4603b0
Alchie Tagudin <alchie.tagudin@thepetlabco.com>
2023-04-10 18:31:46 +0800

    updates

--- create.py
+++ create.py
@@ -9,20 +9,32 @@ if not os.path.isdir("repo"):
 
 for x in folders:
     line = x.strip()
-    print("Directory '% s' created" % line)
+    
+
+    repoLink = "https://github.com/Free-Templates-cc/% s.git" % line
     folder = os.path.join("repo",line)
     
     if not os.path.isdir(folder):
+        print("Directory '% s' created" % line)
         os.makedirs(folder)
+    else:
+        print("Directory '% s' already exist" % line)
+    
+    readmeFile = os.path.join(folder,"README.md")
+    if not os.path.exists(readmeFile):
+        print("README.md File for '% s' created" % line)
+        f = open(readmeFile, "a")
+        f.write("# % s" % line)
+        f.close()
 
-    print("README.md File for '% s' created" % line)
-    f = open(os.path.join(folder,"README.md"), "a")
-    f.write("# % s" % line)
-    f.close()
-
-    subprocess.run(["cd", folder])
-    subprocess.run(["git", "init"])
-    subprocess.run(["git", "add", "."])
-    subprocess.run(["git", "commit", "-m", "\"initial commit\""])
-    subprocess.run(["git", "branch", "-M", "main"])
-    subprocess.run(["git", "remote", "add", "origin","https://github.com/Free-Templates-cc/sintec-nextjs-starter-template.git"])
+    gitFolder = os.path.join(folder,".git")
+    if not os.path.isdir(gitFolder):
+        print("Initializing Git in '% s' created" % line)
+        subprocess.run(["git", "init"], cwd=folder)
+        subprocess.run(["git", "branch", "-M", "main"], cwd=folder)
+        subprocess.run(["git", "add", "."], cwd=folder)
+        subprocess.run(["git", "commit", "-m", "\"initial commit\""], cwd=folder)
+        
+        print(repoLink)
+        subprocess.run(["git", "remote", "add", "origin", repoLink], cwd=folder)
+        subprocess.run(["git", "push", "-u", "origin", "main"], cwd=folder)
